f11b86914a668ba8dba8ff91a30165301cf1da1b
dyf <dyf@fake.email>
2021-10-14 08:40:17 -0700

    organizing a bit

---
+++ examples.py
@@ -0,0 +1,45 @@
+import cfd
+import fvis
+
+def cavity_flow():
+    space = cfd.Space([91,91], [1,1])
+    fluid = cfd.Fluid(space, rho=1, nu=0.05)
+    dx = space.delta[0]
+    u_lid = 1.0 
+    cfl_dt = min(0.25*dx*dx/fluid.nu, 4.0*fluid.nu/u_lid/u_lid)
+
+    fluid.add_boundary_conditions('u', [
+        cfd.DirichletBoundary(dim=0, v=0, end=cfd.Boundary.MIN),
+        cfd.DirichletBoundary(dim=0, v=u_lid, end=cfd.Boundary.MAX), # lid driven 
+        cfd.DirichletBoundary(dim=1, v=0, end=cfd.Boundary.MIN),
+        cfd.DirichletBoundary(dim=1, v=0, end=cfd.Boundary.MAX),
+    ])
+
+    fluid.add_boundary_conditions('v', [
+        cfd.DirichletBoundary(dim=0, v=0, end=cfd.Boundary.MIN),
+        cfd.DirichletBoundary(dim=0, v=0, end=cfd.Boundary.MAX),
+        cfd.DirichletBoundary(dim=1, v=0, end=cfd.Boundary.MIN),
+        cfd.DirichletBoundary(dim=1, v=0, end=cfd.Boundary.MAX)
+    ])
+
+    fluid.add_boundary_conditions('p', [
+        cfd.NeumannBoundary(dim=1, v=0, end=cfd.Boundary.MAX, delta=space.delta[1]),
+        cfd.NeumannBoundary(dim=0, v=0, end=cfd.Boundary.MIN, delta=space.delta[0]),
+        cfd.NeumannBoundary(dim=1, v=0, end=cfd.Boundary.MIN, delta=space.delta[1]),
+        cfd.DirichletBoundary(dim=0, v=0, end=cfd.Boundary.MAX, delta=space.delta[0])
+    ])
+    
+    def debug(i, u, v, p):
+        pass
+
+    fluid.solve(dt=cfl_dt, its=700, p_tol=1e-3, p_max_its=50, cb=debug)
+    
+    
+    fvis.streamplot(fluid)
+
+if __name__ == "__main__": 
+    cavity_flow()
+    #membrane()
+    
+    
+    
---
+++ fvis.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+
+def streamplot(fluid, fig=None):
+    x,y = fluid.space.coords
+
+    vel = np.sqrt(fluid.u**2 + fluid.v**2)
+    plt.contourf(x,y,fluid.p,cmap='magma')
+    plt.colorbar()
+    #plt.quiver(fluid.u,fluid.v,color='k')
+
+    plt.streamplot(x,y,fluid.u, fluid.v)
+    plt.xlim(x[0,0], x[-1,-1])
+    plt.ylim(y[0,0], y[-1,-1])
+    plt.show()
